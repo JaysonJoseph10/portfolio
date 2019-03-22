2700137e87f0c6b572b1db6b03e10c400f978ee1
Jayson Joseph <jaysonjoseph10@gmail.com>
2019-03-21 23:53:38 -0400

    Added logo favicon. Made changes to the header. Refactored script.js functionality

--- js/script.js
+++ js/script.js
@@ -2,12 +2,18 @@ var menuBtn = document.getElementsByClassName('menu-btn')
 
 var mobileMenu = document.getElementsByClassName('mobile-menu')
 
+var body = document.getElementsByTagName('body')[0]
+
 // var header = document.getElementsByTagName('header')[0]
 
 var btnCLicked = function() {
   mobileMenu[0].classList.toggle('active')
   menuBtn[0].classList.toggle('activeBtn')
-  mobileMenu[0].focus()
+  if (mobileMenu[0].classList.contains('active')) {
+    body.style.overflowY = 'hidden'
+  } else if (mobileMenu[0].classList.contains('active') == false) {
+    body.style.overflowY = 'scroll'
+  }
   // if (mobileMenu[0].onblur() = true) {
   //   mobileMenu[0].classList.toggle('active')
   // }
